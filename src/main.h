f19baf4c24216191b579d37848b95422264c5f25
purrleterian <jcno307@gmail.com>
2023-11-20 21:26:26 -0300

    Removed unused function prototypes

--- src/main.h
+++ src/main.h
@@ -5,6 +5,7 @@ typedef struct {
     int hasWon;
     int wordSize;
     int limbs;
+    int lettersFound;
 
     int attemptsN;
     char *attempted;
@@ -13,11 +14,8 @@ typedef struct {
 
 long getFileSize(FILE *fP);
 void populateWordArray(char **words, FILE *fP);
-void displayWords(char **words, long fileLines);
-void displayRandomWord(char **words, long fileLines);
 void *getRandomWord(char **words, long fileLines);
 void initGame(Game *g, char **words, long fileLines);
 void printLines(const Game *g);
 void submitAttempt(Game *g, char attempt);
-void clearInputStream();
 void displayAttempted(const Game *g);
